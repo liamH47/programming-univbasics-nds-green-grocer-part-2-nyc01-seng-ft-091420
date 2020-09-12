8b9cda3227e5e6b5147a6e2e745279a4bf93c81c
Liam Hanafee-Areces <liamhanafee@gmail.com>
2020-09-12 19:10:30 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,11 +1,44 @@
 require_relative './part_1_solution.rb'
+def find_item_by_name_in_collection(name, collection)
+  index = 0
 
+  collection.each do |grocery_item|
+    return grocery_item if grocery_item[:item] === name 
+    index += 1
+  end
+
+  nil
+end
+
+
+def consolidate_cart(cart)
+  index = 0
+  new_cart = []
+  
+  cart.each do |grocery_item|
+    current_item = find_item_by_name_in_collection(grocery_item[:item], new_cart)
+    if current_item
+      new_cart_index = 0
+      new_cart.each do |new_cart_item|
+        if new_cart_item[:item] === current_item[:item]
+          new_cart_item[:count] += 1
+        end
+        new_cart_index += 1
+      end
+    else
+      grocery_item[:count] = 1
+      new_cart << grocery_item
+    end
+    index += 1
+  end
+  new_cart
+end
 
 
 def apply_coupons(cart, coupons)
   counter = 0
   while counter < coupons.length
-  cart_item = find_item_by_name_in_collec
+  cart_item = find_item_by_name_in_collection
   counter += 1
 end
   
