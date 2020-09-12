2f3348c69b95b96c7e889099100c80aaf27a0f9f
Liam Hanafee-Areces <liamhanafee@gmail.com>
2020-09-12 19:09:46 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,7 +1,10 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
-  
+  counter = 0
+  while counter < coupons.length
+  cart_item = find_item_by_name_in_collec
+  counter += 1
 end
   
 def apply_clearance(cart)
