395dc83630afd4e40482cdd032387275c8fb2856
Liam Hanafee-Areces <liamhanafee@gmail.com>
2020-09-12 19:14:09 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -38,7 +38,9 @@ end
 def apply_coupons(cart, coupons)
   counter = 0
   while counter < coupons.length
-  cart_item = find_item_by_name_in_collection
+  cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
+  coupon_item_name = "#{coupons[counter][:item]} W/COUPON"
+  cart_item_with_coupon = find_item_by_name_in_collection(coupon_item_name, cart)
   counter += 1
 end
   
